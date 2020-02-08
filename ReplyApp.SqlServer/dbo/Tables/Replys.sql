df6816b00688344ae983d3fdb6a017d9d79ef304
RedPlus <redplus@redplus.net>
2020-02-09 08:04:14 +0900

    코드 정리

--- ReplyApp.SqlServer/dbo/Tables/Replys.sql
+++ ReplyApp.SqlServer/dbo/Tables/Replys.sql
@@ -7,9 +7,11 @@ CREATE TABLE [dbo].[Replys]
 	[Name] NVarChar(255) Not Null,						-- 작성자
 	[Title] NVarChar(255) Null,							-- 제목
 	[Content] NVarChar(Max) Null,						-- 내용
-	[IsPinned] Bit Null Default(0),						-- 공지글로 올리기 
+
+	[IsPinned] Bit Null Default(0),						-- 공지글로 올리기, IsActive 
+
 	[CreatedBy] NVarChar(255) Null,						-- 등록자(Creator)
-	[Created] DateTime Default(GetDate()) Null,  		-- 생성일(PostDate)
+	[Created] DateTime Default(GetDate()) Null,  		-- 생성일(PostDate), DatePublished
 	[ModifiedBy] NVarChar(255) Null,					-- 수정자(LastModifiedBy)
 	[Modified] DateTime Null,							-- 수정일(LastModified)
 
